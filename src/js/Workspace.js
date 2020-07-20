677d309e8d9fb291390ba4d37d76050b31e5a84d
Wojciech Liana <wojtek.liana@gmail.com>
2020-07-20 21:20:40 +0200

    workspace with divs

--- src/js/Workspace.js
+++ src/js/Workspace.js
@@ -1,7 +1,7 @@
 export function Workspace() {
 
-    this.workspaceImageCanvas = document.querySelector(".workspace-image");
-    this.workspaceLogoCanvas = document.querySelector(".workspace-logo");
+    this.workspacePictureDiv = document.querySelector(".workspace-image");
+    this.workspaceLogoDiv = document.querySelector(".workspace-logo");
     this.workspaceClearBtn = document.querySelector(".work-space__panel__clear");
     this.workspaceSheet = document.querySelector(".work-space__sheet");
     this.workspaceLogo = "";
@@ -11,44 +11,52 @@ export function Workspace() {
 
 Workspace.prototype.addLogoToWorkspace = function (logo) {
 
-    Workspace.prototype.adjustImageSize.bind(this, logo.naturalWidth, logo.naturalHeight, this.workspaceLogoCanvas)();
-    this.workspaceLogo = logo;
-    const ctx = this.workspaceLogoCanvas.getContext("2d");
-    ctx.drawImage(logo, 0, 0);
-
+    this.workspaceLogo = logo.cloneNode(true);
+    this.workspaceLogoDiv.appendChild(this.workspaceLogo);
+    const scale = Workspace.prototype.calculateScaleLogo.bind(this, logo)();
+    Workspace.prototype.adjustLogoSize.bind(this, logo.naturalWidth, logo.naturalHeight, scale)();
 };
 
 Workspace.prototype.addPictureToWorkspace = function (picture) {
 
-    Workspace.prototype.adjustImageSize.bind(this, picture.naturalWidth, picture.naturalHeight, this.workspaceImageCanvas)();
-    this.workspaceLogo = picture;
-    const ctx = this.workspaceImageCanvas.getContext("2d");
-    ctx.drawImage(picture, 0, 0);
-    const scale = Workspace.prototype.calculateScale(picture, this.workspaceLogo);
+    this.workspacePicture = picture.cloneNode(true);
+    this.workspacePictureDiv.appendChild(this.workspacePicture);
+    const scale = Workspace.prototype.calculateScalePicture.bind(this, picture)();
+    Workspace.prototype.adjustImageSize.bind(this, picture.naturalWidth, picture.naturalHeight, scale)();
 
 };
 
-Workspace.prototype.adjustImageSize = function (width, height, canvas) {
+Workspace.prototype.adjustImageSize = function (width, height, scale) {
 
-    canvas.style.width = "100%";
-    canvas.style.height = "100%";
-    canvas.width = width;
-    canvas.height = height;
+    this.workspacePicture.style.width = `${width / scale}px`;
+    this.workspacePicture.style.height = `${height / scale}px`;
 
 };
 
-Workspace.prototype.calculateScale = function (picture, logo) {
+Workspace.prototype.adjustLogoSize = function (width, height, scale) {
 
-    return Math.ceil(picture.naturalHeight / logo.naturalHeight);
+    this.workspaceLogo.style.width = `${width / scale}px`;
+    this.workspaceLogo.style.height = `${height / scale}px`;
+
+};
+
+Workspace.prototype.calculateScalePicture = function (imageToBeScaled) {
+
+    const widthScale = imageToBeScaled.naturalWidth / this.workspacePictureDiv.clientWidth;
+    const heightScale = imageToBeScaled.naturalHeight / this.workspacePictureDiv.clientHeight;
+    return widthScale >= heightScale ? widthScale : heightScale;
+
+};
+
+Workspace.prototype.calculateScaleLogo = function (imageToBeScaled) {
+
+    const widthScale = imageToBeScaled.naturalWidth / this.workspaceLogoDiv.clientWidth;
+    const heightScale = imageToBeScaled.naturalHeight / this.workspaceLogoDiv.clientHeight;
+    return widthScale >= heightScale ? widthScale : heightScale;
 
 };
 
 Workspace.prototype.clearWorkspace = function () {
-
-    const ctxPicture = this.workspaceImageCanvas.getContext("2d");
-    const ctxLogo = this.workspaceLogoCanvas.getContext("2d");
-    ctxPicture.clearRect(0, 0, this.workspaceImageCanvas.width, this.workspaceImageCanvas.height);
-    ctxLogo.clearRect(0, 0, this.workspaceLogoCanvas.width, this.workspaceLogoCanvas.height);
 
 };
 
